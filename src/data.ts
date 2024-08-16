8f3698297713453465f6f2e9eb8f59ff1811b512
chiefportable <kwamea236@protonmail.com>
2024-08-16 12:45:39 +0000

    Add: partials and json data to generate data

---
+++ src/data.ts
@@ -0,0 +1,62 @@
+export const athletes = [
+    {
+        name: "Usain Bolt",
+        event: "100m Sprint",
+        country: "Jamaica",
+    },
+    {
+        name: "Simone Biles",
+        event: "Artistic Gymnastics",
+        country: "USA",
+    },
+    {
+        name: "Mo Farah",
+        event: "5000m",
+        country: "United Kingdom",
+    },
+    {
+        name: "Eliud Kipchoge",
+        event: "Marathon",
+        country: "Kenya",
+    },
+    {
+        name: "Serena Williams",
+        event: "Tennis",
+        country: "USA",
+    },
+    {
+        name: "Caster Semenya",
+        event: "800m",
+        country: "South Africa",
+    },
+    {
+        name: "Michael Phelps",
+        event: "Swimming",
+        country: "USA",
+    },
+    {
+        name: "Yelena Isinbayeva",
+        event: "Pole Vault",
+        country: "Russia",
+    },
+    {
+        name: "Shelly-Ann Fraser-Pryce",
+        event: "100m Sprint",
+        country: "Jamaica",
+    },
+    {
+        name: "Novak Djokovic",
+        event: "Tennis",
+        country: "Serbia",
+    },
+    {
+        name: "Allyson Felix",
+        event: "400m",
+        country: "USA",
+    },
+    {
+        name: "Haile Gebrselassie",
+        event: "10,000m",
+        country: "Ethiopia",
+    },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,40 @@
 import { Handlebars, HandlebarsConfig } from "$handlebars";
+import { athletes } from "./data.ts";
 
-const handlebars = new Handlebars();
+const DEFAULT_HANDLEBARS_CONFIG: HandlebarsConfig = {
+    baseDir: "views",
+    extname: ".hbs",
+    layoutsDir: "layouts/",
+    partialsDir: "partials/",
+    cachePartials: false,
+    defaultLayout: "main",
+    helpers: undefined,
+    compilerOptions: undefined,
+};
 
-const result: string = await handlebars.renderView("index", { name: "Alonso" });
+const handlebars = new Handlebars(DEFAULT_HANDLEBARS_CONFIG);
 
-Deno.serve((req: Request) => {
-    return new Response(result, {
-        status: 200,
+Deno.serve({ port: 8000, hostname: "127.0.0.1" }, async (req: Request) => {
+    const { method, url } = req;
+
+    const newUrl = new URL(url);
+
+    if (newUrl.pathname === "/" && method === "GET") {
+        const result: string = await handlebars.renderView("home", {
+            athlete: athletes[Math.floor(Math.random() * athletes.length)],
+            name: "Raymond Ghanney",
+        });
+        return new Response(result, {
+            headers: {
+                "content-type": "text/html",
+            },
+        });
+    }
+
+    // Handling error 404
+    const index: string = await handlebars.renderView("index");
+    return new Response(index, {
+        status: 404,
         headers: {
             "content-type": "text/html",
         },
